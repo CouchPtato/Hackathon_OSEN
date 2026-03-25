d91feedc29c48048719c1c5033521b86a4f57a5d
CouchPtato <singhsomay467@gmail.com>
2026-03-25 07:45:14 +0530

    Stop tracking generated artifacts

--- .next/server/pages/_app.js
+++
@@ -1,4 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js")
-R.c("server/chunks/ssr/[root-of-the-server]__50372115._.js")
-R.m("[project]/node_modules/.pnpm/next@15.5.14_react-dom@19.2.4_react@19.2.4__react@19.2.4/node_modules/next/app.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/node_modules/.pnpm/next@15.5.14_react-dom@19.2.4_react@19.2.4__react@19.2.4/node_modules/next/app.js [ssr] (ecmascript)").exports
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"xJtrZNJvZhDtwG1Vvqngh4B4OkBMM7psOcbVSVbTeIw=\"\n}"
